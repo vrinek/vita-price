e8ccdd975a780900133796cdecb556c3e973154c
Kostas Karachalios <vrinek@me.com>
2013-12-03 19:35:59 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VitaPrice::Application.config.secret_key_base = '83bb1a8be9f6b27190a60a50481cea390849689ff8806950603c61eed5579c6b6347700a3219131c637fdbcce72de2e9a0cde7963ddb5fa2fcf2c6ba2f571ad6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+VitaPrice::Application.config.session_store :cookie_store, key: '_vita_price_session'
